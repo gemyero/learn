95804456b83ca65e28e122d994b5aa3b4da09693
Mohamed Gamal <m.gemyero91@gmail.com>
2018-04-27 02:40:44 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const app = express();
+
+// views setting
+app.set('view engine', 'hbs');
+
+// static files
+app.use('/assets', express.static(path.join(__dirname, 'public')));
+
+// middlewares
+app.use((req, resp, next) => {
+  console.log('\n#####################################################');
+  console.log(`Requested URL: ${req.url}`);
+  console.log(`Request Time: ${new Date().toString()}`);
+  console.log('#####################################################\n');
+  next();
+});
+
+// routes 
+app.get('/', (req, resp) => {
+  resp.render('home.hbs', {
+    title: 'Home',
+    pageTitle: "Home Page",
+    currnetYear: new Date().getFullYear(),
+    name: "Mohamed Gamal Rady",
+    age: 26
+  });
+});
+
+app.get('/about', (req, resp) => {
+  resp.render('about.hbs', {
+    pageTitle: "About Mohamed Gamal",
+    currnetYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000, () => console.log('Server is listening on localhost:3000'));
