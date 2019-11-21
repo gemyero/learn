b741798ea140cedadb5d98d69f61a552be250669
Mohamed Gamal <mohamed.rady@fixedmea.com>
2019-11-21 14:00:48 +0200

    modify code

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
-// views setting
-app.set('view engine', 'hbs');
-
-// static files
-app.use('/assets', express.static(path.join(__dirname, 'public')));
-
-// middlewares
-app.use((req, resp, next) => {
-  console.log('\n#####################################################');
-  console.log(`Requested URL: ${req.url}`);
-  console.log(`Request Time: ${new Date().toString()}`);
-  console.log('#####################################################\n');
-  next();
+app.get('/', (req, res) => {
+  res.send({ name: 'Mohamed Gamal', age: 28 });
 });
 
-// routes 
-app.get('/', (req, resp) => {
-  resp.render('home.hbs', {
-    title: 'Home',
-    pageTitle: "Home Page",
-    currnetYear: new Date().getFullYear(),
-    name: "Mohamed Gamal Rady",
-    age: 28
-  });
-});
-
-app.get('/about', (req, resp) => {
-  resp.render('about.hbs', {
-    pageTitle: "About Mohamed Gamal Rady",
-    currnetYear: new Date().getFullYear()
-  });
-});
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(3000, () => console.log('Server starts...'));
